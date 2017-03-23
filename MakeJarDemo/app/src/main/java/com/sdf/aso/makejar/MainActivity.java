5b47d57da0edc890372c062115410a1fd528fa14
itsdf07 <923255742@qq.com>
2017-03-23 09:28:21 +0800

    在common module中增加点代码，供app module测试使用

--- MakeJarDemo/app/src/main/java/com/sdf/aso/makejar/MainActivity.java
+++ MakeJarDemo/app/src/main/java/com/sdf/aso/makejar/MainActivity.java
@@ -2,6 +2,9 @@ package com.sdf.aso.makejar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.sdf.aso.common.TestUtils;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("dfsu", "测试Log ：" + TestUtils.getTestMsg());
     }
 }
---
+++ MakeJarDemo/common/src/main/java/com/sdf/aso/common/TestUtils.java
@@ -0,0 +1,15 @@
+package com.sdf.aso.common;
+
+/**
+ * Created by itsdf07 on 2017/3/23 09:24.
+ * E-Mail: 923255742@qq.com
+ * GitHub: https://github.com/itsdf07
+ */
+
+public class TestUtils {
+
+    public static String getTestMsg() {
+        String testMsg = "congratulations ...";
+        return testMsg;
+    }
+}
